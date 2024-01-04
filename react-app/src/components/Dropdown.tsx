9767ed8020b45f6d89842065af0e14666f7c6810
Martin Felber <martin.felber@sap.com>
2024-01-04 13:59:54 +0100

    added dropdown to test my react skills :)

---
+++ react-app/src/components/Dropdown.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useState } from "react";
+
+interface Props {
+  items: Array<string>;
+}
+
+function Dropdown({items}: Props) {
+  const [selectedFile, setSelectedFile] = useState("");
+
+  return (
+    <div className="dropdown">
+      <button
+        className="btn btn-secondary dropdown-toggle"
+        type="button"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        {selectedFile || "Select a file"}
+      </button>
+      <ul className="dropdown-menu">
+        {items.map((item) => (
+          <li>
+          <a className={
+            selectedFile === item
+            ? "dropdown-item active"
+            : "dropdown-item"
+          } href="#" onClick={(event) => (
+            setSelectedFile(item)
+          )}>
+            {item}
+          </a>
+        </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Dropdown;
